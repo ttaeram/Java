f4735cdc95f85902e1168c03f180fd1c9849d045
ttaeram <ryu.taeram@gmail.com>
2024-11-01 18:00:00 +0900

    feat: refresh token

--- jwt/src/main/java/com/member/jwt/jwt/JWTFilter.java
+++ jwt/src/main/java/com/member/jwt/jwt/JWTFilter.java
@@ -2,6 +2,7 @@ package com.member.jwt.jwt;
 
 import com.member.jwt.dto.CustomUserDetails;
 import com.member.jwt.entity.UserEntity;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,6 +13,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class JWTFilter extends OncePerRequestFilter {
 
@@ -25,51 +27,59 @@ public class JWTFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        // request에서 Authorization 헤더를 찾음
-        String authorization = request.getHeader("Authorization");
+        // 헤더에서 access에 담긴 토큰 꺼내기
+        String accessToken = request.getHeader("access");
 
-        // Authorization 헤더 검정
-        if (authorization == null || !authorization.startsWith("Bearer ")) {
+        // 토큰이 없으면 다음 필터로
+        if (accessToken == null) {
 
-            System.out.println("token null");
             filterChain.doFilter(request, response);
 
-            // 조건이 해당되면 해당 메서드 종료
             return;
         }
 
-        System.out.println("authorization now");
+        // 토큰 만료 여부 확인, 만료 시 다음 필터로 넘기기 X
+        try {
+            jwtUtil.isExpired(accessToken);
+        } catch (ExpiredJwtException e) {
 
-        // Bearer 부분 제거 후 순수 토큰만 획득
-        String token = authorization.split(" ")[1];
+            // response body
+            PrintWriter writer = response.getWriter();
+            writer.print("access token expired");
 
-        // 토큰 소멸 시간 검증
-        if (jwtUtil.isExpired(token)) {
+            // response status code
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
-            System.out.println("token expired");
-            filterChain.doFilter(request, response);
-
-            // 조건이 해당되면 해당 메서드 종료
             return;
         }
 
-        // 토큰에서 username과 role 획득
-        String username = jwtUtil.getUsername(token);
-        String role = jwtUtil.getRole(token);
+        // 토큰이 access인지 확인
+        String category = jwtUtil.getCategory(accessToken);
 
-        // UserEntity를 생성하여 값 set
+        if (!category.equals("access")) {
+
+            // response body
+            PrintWriter writer = response.getWriter();
+            writer.print("invalid access token");
+
+            // response status code
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+            return;
+        }
+
+        // username, role 값을 획득
+        String username = jwtUtil.getUsername(accessToken);
+        String role = jwtUtil.getRole(accessToken);
+
         UserEntity userEntity = new UserEntity();
         userEntity.setUsername(username);
-        userEntity.setPassword("tempPassword");
         userEntity.setRole(role);
+        CustomUserDetails customUserDetails = new CustomUserDetails(userEntity);
 
-        // UserDetails에 회원 정보 객체 담기
-        CustomUserDetails customUserDetails =new CustomUserDetails(userEntity);
-
-        // 스프링 시큐리티 인증 토큰 생성
         Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
-        // 세션에 사용자 등록
         SecurityContextHolder.getContext().setAuthentication(authToken);
+
         filterChain.doFilter(request, response);
     }
 }
--- jwt/src/main/java/com/member/jwt/jwt/LoginFilter.java
+++ jwt/src/main/java/com/member/jwt/jwt/LoginFilter.java
@@ -2,8 +2,10 @@ package com.member.jwt.jwt;
 
 import com.member.jwt.dto.CustomUserDetails;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -43,19 +45,22 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
 
-        // UserDetails
-        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
-
-        String username = customUserDetails.getUsername();
+        // 유저 정보
+        String username = authentication.getName();
 
         Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
         Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
         GrantedAuthority auth = iterator.next();
-
         String role = auth.getAuthority();
-        String token = jwtUtil.createJwt(username, role, 60 * 60 * 600000L);
 
-        response.addHeader("Authorization", "Bearer " + token);
+        // 토큰 생성
+        String access = jwtUtil.createJwt("access", username, role, 600000L);
+        String refresh = jwtUtil.createJwt("refresh", username, role, 86400000L);
+
+        // 응답 설정
+        response.setHeader("access", access);
+        response.addCookie(createCookie("refresh", refresh));
+        response.setStatus(HttpStatus.OK.value());
     }
 
     @Override
@@ -63,5 +68,16 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
         // 로그인 실패 시 401 응답 코드 반환
         response.setStatus(401);
+    }
+
+    private Cookie createCookie(String key, String value) {
+
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(24*60*60);
+//        cookie.setSecure(true);
+//        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+
+        return cookie;
     }
 }
